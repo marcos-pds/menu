79998e3d1998bfacbf509c596acf1a42c02c3166
marcos-pds <marcos.code0@gmail.com>
2024-10-31 16:37:32 -0300

    Delete menu-1 directory

--- menu-1/js/ativar.js
+++
@@ -1,8 +0,0 @@
-const list = document.querySelectorAll('.Navegação-lista');
-function activeLink(){
-  list.forEach((item) =>
-item.classList.remove('ativo'));
-this.classList.add('ativo');
-}
-list.forEach((item)=>
-item.addEventListener('click', activeLink));
